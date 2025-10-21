d2c840cc188a7d00e3f06f704ff2f74eb0620864
Aakash Yadav <104193032+dreamofyadav@users.noreply.github.com>
2025-10-21 10:33:08 +0530

    Implement loading state in Home component
    Added loading state to manage fetching blogs.

--- Frontend/src/common/Home.jsx
+++ Frontend/src/common/Home.jsx
@@ -7,22 +7,32 @@ import BlogCard from './BlogCard';
 
 export default function Home() {
       const [blogs, setBlogs] = useState([]);
-
+      const [loading, setLoading] = useState(true);
        // Fetch blogs from backend
 useEffect(() => {       
   const fetchBlogs = async () => {
     try {
+      setLoading(true);
       const res = await axios.get("https://blogpost-q2as.onrender.com/blogs", );
       console.log(res.data);
       setBlogs(res.data.posts); // backend returns array of blog objects
     } catch (err) {
       console.error(err);
     }
+      finally {
+       setLoading(false); // Stop loading
+    }
   };
 
    fetchBlogs();
   }, []);
-
+if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return (
     <div>
       <h1 className='text-3xl my-3 text-gray-700 font-bold text-center sm:text-start ml-10 mt-1 mb-2'>Blogs</h1>
